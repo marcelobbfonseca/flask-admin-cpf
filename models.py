4e4fc534404c2db487cf7b3ea2e2e210512c9c00
marcelobbfonseca <marcelobbff@gmail.com>
2019-03-21 13:20:13 -0300

    fixed validate_cpf returning None

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import event
 from pycpfcnpj import cpfcnpj
-
+from sqlalchemy.orm.session import object_session
 
 db = SQLAlchemy()
 
@@ -10,7 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     rg = db.Column(db.String(20), unique=True, nullable=False)
-    orgao_exp = db.Column(db.String(30), unique=True, nullable=False)
+    orgao_exp = db.Column(db.String(30), nullable=False)
     cpf = db.Column(db.String(14), unique=True, nullable=False)
     email = db.Column(db.String(30), unique=True, nullable=False)
     phone = db.Column(db.String(30))
@@ -18,8 +18,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.name
 
+
 def validate_cpf(target, value, oldvalue, initiator):
     # cpf = value
-    assert cpfcnpj.validate(value), 'CPF inválido!'
+    assert cpfcnpj.validate(value), 'CPF Invalido!'
+    return value
+
 
 event.listen(User.cpf, 'set', validate_cpf, retval=True)
